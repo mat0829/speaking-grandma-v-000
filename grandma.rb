20a90141de0b6c6815b9e705c19b22cad81d52f3
David Anderson <matandersonlv@hotmail.com>
2018-10-04 00:08:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase === phrase.upcase
+  elsif phrase === phrase.upcase()
     return "NO, NOT SINCE 1938!"
   else
     return "I LOVE YOU TOO PUMPKIN!"
